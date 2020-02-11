183ee3c23d40a8e28dac89d303eb1072a7fac8da
Anton Engman <anton.engman@knowit.se>
2020-02-11 12:27:03 +0100

    Add scoreboard logic and view

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import logo from './2_experience_white.png';
+import logo from './2_experience_black.png';
 
 class App extends Component {
 
@@ -10,15 +10,21 @@ class App extends Component {
     this.interval = null;
     this.challenge = 'Lorem ipsum knowit är bäst dolor sit amet';
     this.localStorageName = 'typochallenge';
+    this.localStorageScoreboard = 'scoreboard';
 
-    if(!this.getWorldRecord()) {
+    if(!this.getWorldRecord() || this.getWorldRecord() != 0) {
       this.saveWorldRecord(60000);
+    }
+
+    if(!this.getScoreboard()){
+      this.saveScoreboard([{time: 60000, name: "Test"}]);
     }
 
     this.state = {
       time: 0,
-      worldRecord: this.getWorldRecord(),
-      newWorldRecord: false
+      worldRecord: this.getScoreboard()[0].time,
+      newWorldRecord: false,
+      scoreboard: this.getScoreboard()
     }
   }
 
@@ -30,14 +36,17 @@ class App extends Component {
   stop() {
     console.log('stop');
     clearInterval(this.interval);
-
+    let scoreboard = this.updateScoreboard(this.state.time);
+    this.setState({scoreboard: scoreboard});
+    this.saveScoreboard(scoreboard);
     if(this.state.time < this.state.worldRecord) {
       console.log('wr');
       this.saveWorldRecord(this.state.time);
       this.setState({
         worldRecord: this.state.time,
-        newWorldRecord: true
+        newWorldRecord: true,
       });
+      this.saveScoreboard(scoreboard);
     }
   }
 
@@ -66,8 +75,21 @@ class App extends Component {
     return !wr ? null : wr;
   }
 
+  saveScoreboard(scoreboard){
+    let json = JSON.stringify(scoreboard);
+    localStorage.setItem(this.localStorageScoreboard, JSON.stringify(scoreboard));
+  }
+
+  getScoreboard(){
+    let sb = localStorage.getItem(this.localStorageScoreboard);
+    if(sb === "undefined")
+      return null;
+    let json = JSON.parse(sb);
+    return json.length > 10 ? null : json;
+  }
+
   formatTime(time) {
-    let multiplier = 100;
+    let multiplier = 60;
 
     let minutes = Math.floor(time / (60 * multiplier));
     let seconds = Math.floor((time - (minutes * 60 * multiplier)) / 60);
@@ -78,6 +100,48 @@ class App extends Component {
     if (centiseconds < 10) centiseconds = `0${centiseconds}`;
 
     return `${minutes}:${seconds}:${centiseconds}`;
+  }
+
+  renderScoreboard(){
+    return(
+      <div>
+        <ul>
+        {console.log(this.state.scoreboard)}
+        {this.state.scoreboard.map((element, index) =>
+          <li key={index}>{element.name} {this.formatTime(element.time)}</li>
+        )}
+        </ul>
+      </div>
+    )
+  }
+
+  updateScoreboard(time){
+    let scoreboard = this.state.scoreboard;
+    if(time < this.state.worldRecord){
+      scoreboard.splice(0, 0, {time: time, name: "Anton"})
+      scoreboard.join();
+      return scoreboard;
+    }
+    var test = this.formatTime(scoreboard[scoreboard.length - 1].time);
+    if(time > scoreboard[scoreboard.length - 1].time)
+      if(scoreboard.length <10){
+        scoreboard[scoreboard.length] = {time: time, name: "Test"}
+        return scoreboard;
+      }
+      else
+        return scoreboard;
+    else if(time < scoreboard[scoreboard.length -1].time){
+        for(let i=1;i<9;i++){
+          if(time <= scoreboard[i].time){
+            scoreboard.splice(i, 0, {time: time, name: "Test2"});
+            scoreboard.join();
+            if(scoreboard.length > 10)
+              scoreboard.pop();
+            return scoreboard;
+          }
+        }
+    }
+    return scoreboard;
   }
 
   handleOnChange(event) {
@@ -108,6 +172,12 @@ class App extends Component {
           
           <div className="display__world-record">
             World record: {this.formatTime(this.state.worldRecord)}
+          </div>
+          <div className="scoreboard-title">
+            Top 10:
+            <div className="scoreboard">
+            {this.renderScoreboard()}
+          </div>
           </div>
         </div>
         
